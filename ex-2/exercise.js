aaf0b7b524c4f7d63642680b7872cda99a2b03d8
ahlam alshaba <ama1977ln@outlook.com>
2021-02-26 12:59:08 +0100

    solved

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,3 +1,19 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+
+function tellFortune(jobTitle, geoLocation, partner, numKids) {
+    
+   var jobTitle = prompt("write your job that you want to have");
+    var geoLocation = prompt("write where you want to live");
+    var partner = prompt("write a name of a person ");
+    var numKids = prompt("write a number"); 
+    window.alert( 'You will be a ' + jobTitle + ' in ' + geoLocation + ' and married to ' +
+    partner + ' ' + ' with ' + numKids + ' kids.');
+}
+
+
+tellFortune();
+tellFortune();
+tellFortune();
