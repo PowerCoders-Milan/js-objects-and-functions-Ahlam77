45d6e9fc3527c8a717c57b92c8bc502f7a32a614
ahlam alshaba <ama1977ln@outlook.com>
2021-02-26 16:40:49 +0100

    solved

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,3 +1,26 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+var myChoices = ['colors', 'presidents', 'games'];
+
+ colors = {
+    name : "Black",
+    type : "primo"
+
+}
+ presidents = {
+
+    name : " No one",
+    type : "Dead one"
+}
+
+game = {
+
+    name : " King of Avalon",
+    type : "exiting"
+}
+
+window.alert( "My #1 of colors " + colors.name + ".")
+window.alert( "My #1 as president it is the "  + presidents.type + ".")
+window.alert( "My #1 for game" + game.name + ".")
--- ex-4/exercise.js
+++ ex-4/exercise.js
@@ -1,3 +1,15 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+
+var dob = prompt("Please anter you birthday")
+
+function ageCalculator() {
+    var realDate = new Date();
+    return (realDate.getFullYear()-dob)
+}
+
+
+window.alert(ageCalculator())
+
