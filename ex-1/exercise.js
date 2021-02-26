d3d3f44d6dbef2340f3bdcfe76272a509c1ade28
ahlam alshaba <ama1977ln@outlook.com>
2021-02-26 11:57:33 +0100

    done

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,3 +1,16 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+
+
+function sentnce(noun, verb, adverb, adjective) {
+    return noun + " is a wanderful book to " + verb + " when you are  " + adverb + " with this book you going to be very " + adjective
+}
+
+var noun = prompt(" give a name of favorite book  ")
+var verb = prompt("what do you do with the book, wirte a verb")
+var adverb = prompt("would you writ an adverb")
+var adjective = prompt("would you write an adjective referred to the book")
+
+window.alert(sentnce(noun, verb,adverb,adjective))
